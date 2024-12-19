2544ce623275b1b14f31d7471e63113142c5c2c2
sp <tjsvlf0719@naver.com>
2024-12-19 11:24:15 +0900

    CheckBox

--- lib/def2/FormW.dart
+++ lib/def2/FormW.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class FormW extends StatefulWidget {
@@ -9,22 +11,152 @@ class FormW extends StatefulWidget {
 
 class _FormWState extends State<FormW> {
 
+
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                _TextArea()
-              ],
-            )
-          ],
-        ),
-      )
+        home: Scaffold(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _TextArea(),
+              _SliderArea(),
+              _RadioArea(),
+
+
+            ],
+          )
+        ],
+      ),
+    ));
+  }
+}
+class _CheckArea extends StatefulWidget {
+  const _CheckArea({super.key});
+
+  @override
+  State<_CheckArea> createState() => _CheckAreaState();
+}
+
+class _CheckAreaState extends State<_CheckArea> {
+  List<String> _hobbys = ['코딩', '독서', '운동', '수영', '제작'];
+  Map<String, bool> _selectdHobbys = {}; //Map 기본표시
+
+  @override
+  void initState(){
+    print('init');
+    super.initState();
+
+    for(var h in _hobbys){
+      _selectdHobbys[h] = false;
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+
+    return    Row(
+      children: [
+        ..._hobbys.map(
+                (hobby){
+
+              bool _isSelected = _selectdHobbys[hobby] ?? false;
+              return Row(
+                children: [
+                  Checkbox(value: _isSelected ,  activeColor: Colors.lightGreenAccent,onChanged: (value){
+                    setState(() {
+                      _selectdHobbys[hobby] = value ?? false;
+                    });
+                  }),
+                  Text(hobby,
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: _isSelected ? Colors.red : Colors.black26,
+                        fontWeight: _isSelected ? FontWeight.bold : FontWeight.normal),)
+                ],
+              );
+
+            }
+        )
+      ],
+    );
+  }
+}
+
+
+class _RadioArea extends StatefulWidget {
+  const _RadioArea({super.key});
+
+  @override
+  State<_RadioArea> createState() => _RadioAreaState();
+}
+
+class _RadioAreaState extends State<_RadioArea> {
+  List<String> cities = ['서울', '대전', '부산', '인천'];
+  String? _citieValue = null;
+  @override
+  Widget build(BuildContext context) {
+    return  Row(
+      children: [
+        ...cities.map((city) {
+          bool _isSelectedCity = (city == _citieValue);
+          return Row(
+            children: [
+              Radio<String>(
+                value: city,
+                groupValue: _citieValue,
+                activeColor: Colors.greenAccent,
+                onChanged: (value) {
+                  setState(() {
+                    _citieValue = value ?? '서울';
+                  });
+                },
+              ),
+              Text(
+                city,
+                style: TextStyle(
+                    fontSize: 20,
+                    color: _isSelectedCity ? Colors.red : Colors.black26,
+                    fontWeight: _isSelectedCity ? FontWeight.bold : FontWeight.normal),
+              )
+            ],
+          );
+        })
+      ],
+    );
+  }
+}
+
+class _SliderArea extends StatefulWidget {
+  const _SliderArea({super.key});
+
+  @override
+  State<_SliderArea> createState() => _SliderAreaState();
+}
+
+class _SliderAreaState extends State<_SliderArea> {
+  double _phoneNumber = 100000000;
+
+  @override
+  Widget build(BuildContext context) {
+    return Slider(
+      value: _phoneNumber,
+      onChanged: (value) {
+        setState(() {
+          _phoneNumber = value; // null 체크 없이 value 사용
+        });
+      },
+      min: 100000000,
+      // 최소값
+      max: 109999999,
+      // 최대값
+      divisions: 999999,
+      // divisions 값을 999999로 조정
+      label: _phoneNumber.toStringAsFixed(0), // 소수점 없이 출력
     );
   }
 }
@@ -37,7 +169,6 @@ class _TextArea extends StatefulWidget {
 }
 
 class _TextAreaState extends State<_TextArea> {
-
   String? _userName = null;
   String? _userPassword = null;
 
@@ -51,7 +182,7 @@ class _TextAreaState extends State<_TextArea> {
           child: TextFormField(
             decoration: InputDecoration(label: Text('이름 입력하기')),
             keyboardType: TextInputType.text,
-            onChanged: (value){
+            onChanged: (value) {
               setState(() {
                 _userName = value ?? '';
               });
@@ -66,7 +197,7 @@ class _TextAreaState extends State<_TextArea> {
             decoration: InputDecoration(label: Text('비밀번호 입력하기')),
             obscureText: true,
             keyboardType: TextInputType.text,
-            onChanged: (value){
+            onChanged: (value) {
               setState(() {
                 _userPassword = value ?? '';
               });
@@ -74,11 +205,13 @@ class _TextAreaState extends State<_TextArea> {
             },
           ),
         ),
-        ElevatedButton(onPressed: (){
-          print('========================');
-          print('userName: ${_userName}');
-          print('userPassword: ${_userPassword}');
-        }, child: Text('Check!'))
+        ElevatedButton(
+            onPressed: () {
+              print('========================');
+              print('userName: ${_userName}');
+              print('userPassword: ${_userPassword}');
+            },
+            child: Text('Check!'))
       ],
     );
   }
